becaa276af7d5a5d420ea8128ec9b977bf863c9a
Moussab <nabouzi.moussab@gmail.com>
2024-10-27 17:00:58 +0100

    Add: SearchService, Endpoint  synchronous with AuctionSrv

---
+++ src/SearchService/Services/AuctionSvcHttpClient.cs
@@ -0,0 +1,42 @@
+﻿using MongoDB.Entities;
+using SearchService.Models;
+using System.Net.Http;
+
+namespace SearchService.Services
+{
+    public class AuctionSvcHttpClient
+    {
+        private readonly HttpClient _httpClient;
+        private readonly IConfiguration _config;
+
+        public AuctionSvcHttpClient(HttpClient httpClient, IConfiguration config)
+        {
+            _httpClient = httpClient;
+            _config = config;
+        }
+
+        public async Task<List<Item>> GetItemsForSearchDb()
+        {
+            var lastUpdated = await DB.Find<Item, string>()
+                .Sort(x => x.Descending(x => x.UpdatedAt))
+                .Project(x => x.UpdatedAt.ToString())
+                .ExecuteFirstAsync();
+
+
+            var auctionURL = _config["AuctionServiceUrl"]
+                ?? throw new ArgumentNullException("Cannot get auction address");
+
+            var url = auctionURL + "/api/auctions";
+
+            if (!string.IsNullOrEmpty(lastUpdated))
+            {
+                url += $"?date={lastUpdated}";
+            }
+
+            var items = await _httpClient.GetFromJsonAsync<List<Item>>(url);
+
+            return items ?? [];
+
+        }
+    }
+}
