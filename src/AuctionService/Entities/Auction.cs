c0d11e594050af2f72bba5ba6ff42b18c7ddea52
Moussab <nabouzi.moussab@gmail.com>
2024-10-27 11:39:29 +0100

    InitialCommit: AuctionService

---
+++ src/AuctionService/Entities/Auction.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AuctionService.Entities
+{
+    [Table("Auctions")]
+    public class Auction
+    {
+        public Guid Id { get; set; }
+        public int ReservePrice { get; set; } = 0;
+        public string Seller { get; set; }
+        public string? Winner { get; set; }
+        public int? SoldAmount { get; set; }
+        public int? CurrentHignBid { get; set; }
+        public DateTime CreateAt { get; set; } = DateTime.UtcNow;
+        public DateTime UpdateAt { get; set; } = DateTime.UtcNow;
+        public DateTime AuctionEnd { get; set; }
+        public Status Status { get; set; }
+        public Item Item { get; set; }
+
+    }
+}
